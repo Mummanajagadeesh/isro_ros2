9d66d922e94727ba0382cca38b75410b2d7dadca
nnirann <niraamaymd@gmail.com>
2025-05-17 17:43:35 +0530

    stopped tracking build log ...

--- ros_ws/build/camera_feed_pub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/niraamay/isro_ros2_try/ros_ws/src/camera_feed_pub
-# Build directory: /home/niraamay/isro_ros2_try/ros_ws/build/camera_feed_pub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
