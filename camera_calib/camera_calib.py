a0150fbf70f1beae3cb2929d8f5f8801804033f9
nnirann <niraamaymd@gmail.com>
2025-05-20 23:09:02 +0530

    rpicamv2 calib

--- camera_calib/camera_calib.py
+++ camera_calib/camera_calib.py
@@ -13,7 +13,7 @@ objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-images = glob.glob('test_images/*.jpeg')
+images = glob.glob('rpicam_v2_imgs/*.png')
 
 for fname in images:
     img = cv.imread(fname)
